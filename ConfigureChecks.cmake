f905cbe6e2ef7f6e6de33ac9373a263f977e37c3
Ivailo Monev <xakepa10@gmail.com>
2015-11-03 14:41:34 +0200

    generic: build system cleanups
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- ConfigureChecks.cmake
+++ ConfigureChecks.cmake
@@ -7,6 +7,3 @@ include(CheckPrototypeExists)
 include(CheckStructMember)
 include(CheckTypeSize)
 include(CheckCXXSourceCompiles)
-
-check_function_exists(getpeereid  HAVE_GETPEEREID) # kdesu
-check_function_exists(statvfs HAVE_STATVFS) # kinfocenter
