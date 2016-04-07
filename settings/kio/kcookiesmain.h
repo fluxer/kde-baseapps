06fff783213d16c47808a70b6d964d335e5033d7
Ivailo Monev <xakepa10@gmail.com>
2016-04-07 03:22:58 +0000

    generic: replace konqueror with rekonq
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

---
+++ settings/kio/kcookiesmain.h
@@ -0,0 +1,40 @@
+/*    kcookiesmain.h - Cookies configuration
+ *
+ *    First version of cookies configuration:
+ *        Copyright (C) Waldo Bastian <bastian@kde.org>
+ *    This dialog box:
+ *        Copyright (C) David Faure <faure@kde.org>
+ *
+ */
+
+#ifndef KCOOKIESMAIN_H
+#define KCOOKIESMAIN_H
+
+#include <kcmodule.h>
+
+class QTabWidget;
+class KCookiesPolicies;
+class KCookiesManagement;
+
+class KCookiesMain : public KCModule
+{
+    Q_OBJECT
+public:
+    KCookiesMain(QWidget *parent, const QVariantList &args);
+    ~KCookiesMain();
+
+    KCookiesPolicies* policyDlg() { return policies; }
+
+    virtual void save();
+    virtual void load();
+    virtual void defaults();
+    virtual QString quickHelp() const;
+
+private:
+
+    QTabWidget* tab;
+    KCookiesPolicies* policies;
+    KCookiesManagement* management;
+};
+
+#endif // KCOOKIESMAIN_H
