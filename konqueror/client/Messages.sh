fdf00347d6afb36579152bcf8b42571e6b094893
Ivailo Monev <xakepa10@gmail.com>
2016-04-07 04:08:41 +0000

    generic: replace konqueror with rekonq
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- konqueror/client/Messages.sh
+++
@@ -1,2 +0,0 @@
-#! /usr/bin/env bash
-$XGETTEXT -kaliasLocal *.h *.cpp -o $podir/kfmclient.pot
--- konqueror/settings/konqhtml/Messages.sh
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/env bash
-$EXTRACTRC `find . -name \*.ui` >> rc.cpp
-$XGETTEXT *.cpp css/*.cpp -o $podir/kcmkonqhtml.pot
